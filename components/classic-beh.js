bb2513edafe4880b0ae9f9333f9e0f947c59fa7a
cf88s8 <44262927+cf88s8@users.noreply.github.com>
2019-07-13 17:59:27 +0800

    Add files via upload

---
+++ components/classic-beh.js
@@ -0,0 +1,31 @@
+let classicBeh=Behavior({
+  /**
+ * 组件的属性列表
+ */
+  properties: {
+    img:String,
+    content:String,
+    hidden:Boolean
+  },
+
+attached:function(){
+
+},
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
+
+// 行为组建 ，即复用组建，允许被COMPONENTS组件复用  需在使用处
+// 
+export{classicBeh}
