c96b3b7c6c7405138edccc01341d8496652da6e5
cf88s8 <44262927+cf88s8@users.noreply.github.com>
2019-07-13 18:37:38 +0800

    Add files via upload

---
+++ components/image-button/index.js
@@ -0,0 +1,36 @@
+// components/image-button/index.js
+Component({
+
+  // 启用插槽
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    openType:{
+      type:String,
+
+    },
+    name:{
+      type:String,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onGetUserInfo(event){
+      this.triggerEvent('getuserinfo',event.detail,{})
+    }
+  }
+})
